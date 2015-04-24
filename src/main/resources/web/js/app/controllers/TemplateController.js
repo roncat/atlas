d5aad2ec2dbd0c253d9d55a42c7cf5a307ad3d7c
Carlos Alberto <carlos.alberto@aexo.com.br>
2015-04-24 17:53:50 -0300

    template saving  now

--- src/main/resources/web/js/app/controllers/TemplateController.js
+++ src/main/resources/web/js/app/controllers/TemplateController.js
@@ -13,6 +13,20 @@ define([ './controllers' ], function(controllers) {
 			$scope.$safeApply();
 		})
 		
+		$scope.saveScript = function(){
+			$.ajax({
+				url : "/template",
+				type : "POST",
+				data : $scope.script,
+				contentType : "text/plain; charset=utf-8",
+				dataType : "text"
+			}).done(function(){
+				alert("Template script saved");
+			}).fail(function(){
+				alert("error in save script");
+			});
+		}
+		
 		
 		$scope.testScript = function(){
 			$.ajax({
